7f1155dcc87f59c44173d1d4cc391a8e0772cd22
nirbhaykumarsaini <140544127+nirbhaykumarsaini@users.noreply.github.com>
2023-09-18 10:13:47 +0530

    routing
    done

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -12,10 +13,12 @@ import { IonicModule } from '@ionic/angular';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-  
+  goBack(){
+    this.router.navigate(["/master"])
+  }
 
 }
