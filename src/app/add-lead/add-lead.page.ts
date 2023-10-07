1b3f60507ea19a6e20ea27f26877f1483dc642f9
nirbhaykumarsaini <nirbhaykumarsaini@gmail.com>
2023-10-07 12:47:30 +0530

    chnaegs
    done

--- src/app/add-lead/add-lead.page.ts
+++ src/app/add-lead/add-lead.page.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-lead',
   templateUrl: './add-lead.page.html',
   styleUrls: ['./add-lead.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class AddLeadPage implements OnInit {
 
-  constructor(private router: Router) { }
+  leadOwner: string = '';
+  cpName: string='';
+  cName: string = '';
+  country: string = '';
+  state: string = '';
+  city: string = '';
+  pinCode: number | null = null;
+  mapTO: string = '';
+  address: string = '';
+  productName: string = '';
+  reference: string = '';
+  email: string = '';
+  mobileNumber: number | null = null;
+  remark: string = '';
+
+  constructor(private router: Router, private toastCtrl: ToastController) { }
+
+  async onSubmit() {
+    
+  }
 
   ngOnInit() {
   }
