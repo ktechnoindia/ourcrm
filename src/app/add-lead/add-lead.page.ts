24e53f1f7d0bfb69a997e6bfcd26d173bfe3a944
nirbhaykumarsaini <nirbhaykumarsaini@gmail.com>
2023-10-16 11:25:36 +0530

    16/10/
    done

--- src/app/add-lead/add-lead.page.ts
+++ src/app/add-lead/add-lead.page.ts
@@ -1,15 +1,28 @@
 // add-lead.page.ts
-import { Component, NgModule } from '@angular/core';
-
-
-
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-lead',
   templateUrl: 'add-lead.page.html',
   styleUrls: ['add-lead.page.scss']
 })
-
 export class AddLeadPage {
-  constructor(){}
+  leadForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.leadForm = this.formBuilder.group({
+      leadOwner: ['', Validators.required],
+      cpName: ['', Validators.required],
+      cName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobileNo: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.leadForm.valid) {
+      // Perform form submission logic here
+    }
+  }
 }
