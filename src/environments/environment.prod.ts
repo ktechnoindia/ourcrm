291d5b008dda8f924aef0e4f42259d98c4d2ba86
ktechnoindia <136716430+ktechnoindia@users.noreply.github.com>
2023-10-18 16:38:46 +0530

    chng

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@
 export const environment = {
   production: true,
   apibaseurl:'http://103.154.184.66:8000/app1/getCountries',
-  
+  fetchallcountries:'getCountries', // Replace with your API's base URL
+  fetchprice: 'prices', // Replace with the endpoint or route you want to use
+  fetchstate:'getStates',
+  fetchdistrict:'getdistrict',
 };
