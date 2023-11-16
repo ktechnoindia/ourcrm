bc7b7eae84c4bf78228d7a915f28f6e3be156202
Yogesh Sharma <yogeshsharma303706@gmail.com>
2023-11-16 12:08:12 +0530

    16/11
    add new pages sale return , purchase return, view sales return, view purchase return

---
+++ src/app/salesreturn/salesreturn.page.ts
@@ -0,0 +1,161 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule, ToastController } from '@ionic/angular';
+import { Router, RouterModule } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { SalesService, salesstore } from '../services/sales.service';
+import { UnitnameService } from '../services/unitname.service';
+import { GsttypeService } from '../services/gsttype.service';
+import { quotestore } from '../services/quotation.service';
+interface Sales {
+  barcode: string;
+  itemcode: number;
+  itemname: number,
+  description: string;
+  quantity: number;
+  unitname: number;
+  mrp: number;
+  basicrate: number;
+  netrate: number;
+  grossrate: number;
+  taxrate: number;
+  CGST: number;
+  SGST: number;
+  IGST: number;
+  discount: number;
+  discountamt: number;
+  totaltax: number;
+  total: number;
+  
+}
+@Component({
+  selector: 'app-salesreturn',
+  templateUrl: './salesreturn.page.html',
+  styleUrls: ['./salesreturn.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule,RouterModule]
+})
+export class SalesreturnPage implements OnInit {
+  billNumber: number | null = null;
+  billDate: string = '';
+  payment: string = '';
+  cName: string = '';
+  orderDate: string = '';
+  orderNumber: number | null = null;
+  gstin: number | null = null;
+  salePerson: string = '';
+  taxrate: string = '';
+  unit: string = '';
+  item: string = '';
+  custcode: string = '';
+  billformate:number=0;
+  custname: string = '';
+  salesData: Sales[] = [{
+    barcode: '',
+    itemcode: 0,
+    itemname: 0,
+    description: '',
+    quantity: 0,
+    unitname: 0,
+    mrp: 0,
+    basicrate: 0,
+    netrate: 0,
+    grossrate: 0,
+    taxrate: 0,
+    CGST: 0,
+    SGST: 0,
+    IGST: 0,
+    discount: 0,
+    discountamt: 0,
+    totaltax: 0,
+    total: 0,
+  }];
+  ttotal!: number;
+  unitname$: any;
+  taxrate$: any;
+  ponumber:string='';
+  refrence:string='';
+  refdate:string='';
+  constructor(private unittype: UnitnameService, private gstsrvs: GsttypeService, private router: Router, private toastCtrl: ToastController, private sales: SalesService) {
+    this.taxrate$ = this.gstsrvs.getgsttype();
+    this.unitname$ = this.unittype.getunits();
+  }
+  onSubmit(myform: NgForm, salesData: any) {
+    console.log('Your form data : ', myform.value);
+    let quotedata: quotestore = {
+      quoteNumber: myform.value.quoteNumber, quateDate: myform.value.quateDate, quoteGroup: myform.value.quoteGroup, quateTax: myform.value.quateTax, item: myform.value.item, taxrate: myform.value.taxrate, description: myform.value.description, quantity: myform.value.quantity, basicrate: myform.value.basicrate, grossrate: myform.value.grossrate, CGST: myform.value.CGST, SGST: myform.value.SGST,
+      payment: myform.value.payment,
+      orderNumber: myform.value.orderNumber,
+      gstin: myform.value.gstin,
+      salePerson: myform.value.salePerson,
+      unit: myform.value.unit,
+      total: myform.value.total,
+      ttotal: myform.value.ttotal
+    };
+    this.sales.createsale(salesData, '', '').subscribe(
+      (response: any) => {
+        console.log('POST request successful', response);
+        // Handle the response as needed
+      },
+      (error: any) => {
+        console.error('POST request failed', error);
+        // Handle the error as needed
+      }
+    );
+  }
+  addSales() {
+      console.log('addrowwww'+this.salesData.length);
+      // You can initialize the new row data here
+      const newRow :Sales= {
+        barcode: '',
+        itemcode: 0,
+        itemname: 0,
+        description:'',
+        quantity:0,
+        unitname:0,
+        mrp:0,
+        basicrate:0,
+        netrate:0,
+        grossrate:0,
+        taxrate:0,
+        CGST:0,
+        SGST:0,
+        IGST:0,
+        discount:0,
+        discountamt:0,
+        totaltax:0,
+        total:0,
+        // Add more properties as needed
+      };
+      this.salesData.push(newRow);
+    }
+    removeSales(index: number,row:Sales) {
+      this.ttotal=this.ttotal-this.sales.total;
+      this.salesData.splice(index, 1);
+    }
+    calculateTotalSum() {
+      let sum = 0;
+      for (const row of this.salesData) {
+        sum += this.sales.total;
+      }
+      this.ttotal= sum;
+    }
+
+    // if (this.form.valid) {
+    //   console.log('Selected Value' + this.form.value);
+    // } else {
+    //   Object.keys(this.form.controls).forEach(controlName => {
+    //     const control = this.form.get(controlName);
+    //     if (control.invalid) {
+    //       control.markAsTouched();
+    //     }
+    //   })
+    // }
+  
+  ngOnInit() {
+  }
+  goBack() {
+    this.router.navigate(["/sales-manager"])
+  }
+}
---
+++ src/app/view-salesreturn/view-salesreturn.page.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-view-salesreturn',
+  templateUrl: './view-salesreturn.page.html',
+  styleUrls: ['./view-salesreturn.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule,RouterLink]
+})
+export class ViewSalesreturnPage implements OnInit {
+  formDate:string='';
+  toDate:string='';
+
+  constructor(private router:Router,private toastCtrl:ToastController) { }
+
+  async onSubmit(){
+    if(this.formDate===''){
+      const toast = await this.toastCtrl.create({
+        message:"Form Date is required",
+        duration:3000,
+        color:'danger',
+      });
+      toast.present();
+    }else if(this.toDate===''){
+      const toast = await this.toastCtrl.create({
+        message:"To Date is required",
+        duration:3000,
+        color:'danger',
+      });
+      toast.present();
+    }else{
+      const toast = await this.toastCtrl.create({
+        message:"Successfully !",
+        duration:3000,
+        color:'success',
+      });
+      toast.present();
+    }
+  }
+
+  ngOnInit() {
+  }
+goBack(){
+  this.router.navigate(["/salesreturn"])
+}
+}
