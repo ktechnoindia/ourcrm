1d38728610b75b3c137100870b8e80002ed4f947
Yogesh Sharma <yogeshsharma303706@gmail.com>
2023-11-22 18:06:12 +0530

    22/11
    add two new pages payment and receipt

---
+++ src/app/receipt/receipt.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-receipt',
+  templateUrl: './receipt.page.html',
+  styleUrls: ['./receipt.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class ReceiptPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
