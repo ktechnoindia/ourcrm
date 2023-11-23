878d4f3913cd41cc4104e8cde0526dd97a919df7
Yogesh Sharma <yogeshsharma303706@gmail.com>
2023-11-23 14:26:22 +0530

    23/11
    edit new design in payment and payment transaction page

--- src/app/paymenttransaction-report/paymenttransaction-report.page.ts
+++ src/app/paymenttransaction-report/paymenttransaction-report.page.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import {Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-paymenttransaction-report',
   templateUrl: './paymenttransaction-report.page.html',
   styleUrls: ['./paymenttransaction-report.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, RouterModule]
 })
 export class PaymenttransactionReportPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
   ngOnInit() {
   }
-
+  goBack() {
+    this.router.navigate(['/payment']); // Navigate back to the previous page
+  }
 }
