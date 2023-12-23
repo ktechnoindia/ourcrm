75ce8eb412c7ad05055c8890d4d0b5318b7ea7d8
gurpreet.singh <csmpl@123>
2023-12-23 08:58:15 +0530

    chngs

--- src/app/interceptors/auth.guard.ts
+++ src/app/interceptors/auth.guard.ts
@@ -2,6 +2,7 @@
 
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { SessionService } from '../services/session.service';
 //import { AuthService } from './auth.service'; // Your authentication service
 
 @Injectable({
@@ -9,14 +10,16 @@ import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from
 })
 export class AuthGuard implements CanActivate {
 
-  constructor( private router: Router) {}
+  constructor( private router: Router,private session :SessionService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
    // if (this.authService.isAuthenticated()) {
+    console.log('in auth guard');
+   const userid = this.session.getValue('userid')?.valueOf()
     console.log('> '+next.url);
-   if(true){  
+   if(userid){  
    return true; // User is authenticated, allow access to the route
     } else {
       this.router.navigate(['/login']); // Redirect to login page if not authenticated
