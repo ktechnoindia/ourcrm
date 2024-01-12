62b6ae99e049ee4d59c171099fd4c855d2c76dd5
Yogesh Sharma <65293781+yogeshsharmao2@users.noreply.github.com>
2024-01-12 16:46:02 +0530

    12/01
    edit new designs in all dashboards

--- src/app/accountdashboard/accountdashboard.page.ts
+++ src/app/accountdashboard/accountdashboard.page.ts
@@ -1,25 +1,143 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
+import { SessionService } from '../services/session.service';
+import Chart from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Colors } from 'chart.js';
+import { PaymentService } from '../services/payment.service';
+import { EncryptionService } from '../services/encryption.service';
+import { RecepitService } from '../services/recepit.service';
+import { Observable } from 'rxjs';
+Chart.register(Colors);
 
 @Component({
   selector: 'app-accountdashboard',
   templateUrl: './accountdashboard.page.html',
   styleUrls: ['./accountdashboard.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class AccountdashboardPage implements OnInit {
   selectedOptions: string[] = [];
+  username: string = 'K-Techno Soft. Pvt. Ltd.';
+  notificationCount: number = 5; // Replace this with the actual notification count
+  openNotificationsPage() {
+    // Implement your logic to open the notifications page or handle notifications
+    // You may want to reset the notification count after viewing the notifications
+    this.notificationCount = 0;
+  }
+  ngAfterViewInit() {
+    const PaymentCanvas: any = document.getElementById('paymentBarChart');
+    const ReceiptCanvas: any = document.getElementById('receiptBarChart');
 
-  constructor() { 
+
+    if (PaymentCanvas) {
+      this.createBarChart(PaymentCanvas, 'Total Payment Transaction', [this.totalpayment]);
+    }
+
+    if (ReceiptCanvas) {
+      this.createBarChart(ReceiptCanvas, 'Total Receipt Transaction', [this.totalreceipt]);
+    }
+  }
+  payment$: Observable<any[]>;
+  totalpayment: number = 0;
+  recepits$: Observable<any[]>;
+  totalreceipt: number = 0;
+  filteredRecepits$: Observable<any[]> = new Observable<any[]>(); 
+  constructor(private recepitService: RecepitService, private paymentservice: PaymentService, private navCtrl: NavController, private session: SessionService, private encService: EncryptionService) {
     this.selectedOptions = ['paymenttransactionlist', 'receipttransactionlist'];
+    const compid = '1';
+    this.payment$ = this.paymentservice.fetchAllPayment(encService.encrypt(compid), '', '');
+    console.log(this.payment$);
+    this.payment$.subscribe((data: string | any[]) => {
+      console.log(data);
+      this.totalpayment = data.length // Log the data to the console to verify if it's being fetched
+    });
+    this.recepits$ = this.recepitService.fetchAllReceppit(encService.encrypt(compid), '', '');
+    console.log(this.recepits$);
+    this.recepits$.subscribe((data: string | any[]) => {
+      console.log(data);
+      this.totalreceipt = data.length // Log the data to the console to verify if it's being fetched
+    });
+  }
+  createBarChart(canvas: any, label: string, data: number[]) {
+    const ctx = canvas.getContext('2d');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: [label],
+        datasets: [{
+          label: label,
+          data: data,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 1,
+        }],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+          x: {
+            beginAtZero: true,
+          },
+        },
+        plugins: {
+          datalabels: {
+            anchor: 'end',
+            align: 'end',
+            color: 'red',
+            font: {
+              weight: 'bold',
+            },
+            formatter: (value: any, context: any) => {
+              return value;
+            },
+          },
+        },
+      },
+    });
+  }
+  async ngOnInit() {
+    //this.username=await this.session.getValue('companyname');
+    const compid = '1';
+
+    //Fetch data for all entities
+    this.payment$ = this.paymentservice.fetchAllPayment(this.encService.encrypt(compid), '', '');
+    this.recepits$ = this.recepitService.fetchAllReceppit(this.encService.encrypt(compid), '', '');
+
+
+    // Subscribe to the observables and update the counts
+    this.payment$.subscribe((data: string | any[]) => {
+      this.totalpayment = data.length;
+      this.updateChartData('paymentBarChart', 'Payment Transaction', [this.totalpayment]);
+    });
+
+    this.recepits$.subscribe((data: string | any[]) => {
+      this.totalreceipt = data.length;
+      this.updateChartData('receiptBarChart', 'Receipt Transaction', [this.totalreceipt]);
+    });
+
 
   }
 
-  ngOnInit() {
+  updateChartData(chartId: string, label: string, data: number[]) {
+    const chart = Chart.getChart(chartId);
+    if (chart) {
+      chart.data.labels = [label];
+      chart.data.datasets[0].data = data;
+      chart.update();
+    }
   }
+  logout() {
+    // Clear authentication tokens or perform other logout logic
+    // this.authService.logout();
 
+    // Navigate to the login page (assuming your login page has a route named 'login')
+    this.navCtrl.navigateRoot('/login');
+  }
 }
--- src/app/transcationdashboard/transcationdashboard.page.ts
+++ src/app/transcationdashboard/transcationdashboard.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
 import { EncryptionService } from '../services/encryption.service';
 import { CustomerService } from '../services/customer.service';
@@ -19,13 +19,14 @@ import { PurchasereturnService } from '../services/purchasereturn.service';
 import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { Colors } from 'chart.js';
+import { SessionService } from '../services/session.service';
 
 @Component({
   selector: 'app-transcationdashboard',
   templateUrl: './transcationdashboard.page.html',
   styleUrls: ['./transcationdashboard.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,RouterLink]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class TranscationdashboardPage implements OnInit {
   ngAfterViewInit() {
@@ -46,55 +47,61 @@ export class TranscationdashboardPage implements OnInit {
       this.createBarChart(totalpurchaseCanvas, 'Total Purchase', [this.totalpurchase]);
     }
   }
-  quote$:  Observable<any[]>;
+  quote$: Observable<any[]>;
   dcin$: Observable<any[]>;
   dcout$: Observable<any>;
   sales$: Observable<any[]>;
   purchase$: Observable<any[]>
   purchasereturn$: Observable<any[]>
-  totalQuote: number=0;
-  totaldcin: number=0;
-  totaldcout: number=0;
-  totalsales: number=0;
-  totalpurchase: number=0;
+  totalQuote: number = 0;
+  totaldcin: number = 0;
+  totaldcout: number = 0;
+  totalsales: number = 0;
+  totalpurchase: number = 0;
   salreturn$: Observable<any[]>;
-  totalsalesreturn: number=0;
-  totalpurchasereturn: number=0;
+  totalsalesreturn: number = 0;
+  totalpurchasereturn: number = 0;
   selectedOptions: string[] = [];
-
-  constructor(private encService: EncryptionService,private quoteservice: QuotationService,private venderService:VendorService,private executService:ExecutiveService,private additem : AdditemService,private dcinservice: DcinService,private dcoutservice:DcoutService,private saleService: SalesService, private purchaseService:PurchaseService,private salereturnservice:SalereturnService,private purchasereturnservice:PurchasereturnService, ) { 
+  username: string = 'K-Techno Soft. Pvt. Ltd.';
+  notificationCount: number = 5; // Replace this with the actual notification count
+  openNotificationsPage() {
+    // Implement your logic to open the notifications page or handle notifications
+    // You may want to reset the notification count after viewing the notifications
+    this.notificationCount = 0;
+  }
+  constructor(private navCtrl: NavController, private session: SessionService, private encService: EncryptionService, private quoteservice: QuotationService, private venderService: VendorService, private executService: ExecutiveService, private additem: AdditemService, private dcinservice: DcinService, private dcoutservice: DcoutService, private saleService: SalesService, private purchaseService: PurchaseService, private salereturnservice: SalereturnService, private purchasereturnservice: PurchasereturnService,) {
     this.selectedOptions = ['saleslist', 'purchaselist'];
     const compid = '1';
     const userid = '1';
-    this.quote$ = this.quoteservice.fetchallQuote(encService.encrypt(compid),(userid), '', '');
+    this.quote$ = this.quoteservice.fetchallQuote(encService.encrypt(compid), (userid), '', '');
     console.log(this.quote$);
 
     this.quote$.subscribe(data => {
       console.log(data); // Log the data to the console to verify if it's being fetched
-      this.totalQuote=data.length;
+      this.totalQuote = data.length;
     });
     this.dcin$ = this.dcinservice.fetchallDcin(encService.encrypt(compid), '', '');
     console.log(this.dcin$);
 
     this.dcin$.subscribe(data => {
       console.log(data); // Log the data to the console to verify if it's being fetched
-      this.totaldcin=data.length;
+      this.totaldcin = data.length;
     });
 
-    this.dcout$ = this.dcoutservice.fetchallDcout(encService.encrypt(compid),'','');
+    this.dcout$ = this.dcoutservice.fetchallDcout(encService.encrypt(compid), '', '');
     console.log(this.dcout$);
 
     this.dcout$.subscribe(data => {
       console.log(data); // Log the data to the console to verify if it's being fetched
-      this.totaldcout=data.length;
+      this.totaldcout = data.length;
     });
-    
+
     this.sales$ = this.saleService.fetchallSales(encService.encrypt(compid), '', '');
     console.log(this.sales$);
 
     this.sales$.subscribe(data => {
       console.log(data); // Log the data to the console to verify if it's being fetched
-      this.totalsales=data.length;
+      this.totalsales = data.length;
 
     });
 
@@ -103,104 +110,111 @@ export class TranscationdashboardPage implements OnInit {
 
     this.purchase$.subscribe(data => {
       console.log(data); // Log the data to the console to verify if it's being fetched
-      this.totalpurchase=data.length;
+      this.totalpurchase = data.length;
 
     });
 
-    this.salreturn$ = this.salereturnservice.fetchallSalesreturn(encService.encrypt(compid),'','');
+    this.salreturn$ = this.salereturnservice.fetchallSalesreturn(encService.encrypt(compid), '', '');
     console.log(this.salreturn$);
 
     this.salreturn$.subscribe(data => {
       console.log(data); // Log the data to the console to verify if it's being fetched
-      this.totalsalesreturn=data.length;
+      this.totalsalesreturn = data.length;
 
     });
-    this.purchasereturn$ = this.purchasereturnservice.fetchallPurchasereturn(encService.encrypt(compid),'','');
+    this.purchasereturn$ = this.purchasereturnservice.fetchallPurchasereturn(encService.encrypt(compid), '', '');
     console.log(this.purchasereturn$);
 
-    this.  purchasereturn$.subscribe(data => {
+    this.purchasereturn$.subscribe(data => {
       console.log(data); // Log the data to the console to verify if it's being fetched
-      this.totalpurchasereturn=data.length;
+      this.totalpurchasereturn = data.length;
     });
 
-    
 
-} createBarChart(canvas: any, label: string, data: number[]) {
-  const ctx = canvas.getContext('2d');
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: [label],
-      datasets: [{
-        label: label,
-        data: data,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor:  'rgb(255, 99, 132)',
-        borderWidth: 1,
-      }],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-        x: {
-          beginAtZero: true,
-        },
+
+  } createBarChart(canvas: any, label: string, data: number[]) {
+    const ctx = canvas.getContext('2d');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: [label],
+        datasets: [{
+          label: label,
+          data: data,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 1,
+        }],
       },
-      plugins: {
-        datalabels: {
-          anchor: 'end',
-          align: 'end',
-          color: 'red',
-          font: {
-            weight: 'bold',
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
           },
-          formatter: (value: any, context: any) => {
-            return value;
+          x: {
+            beginAtZero: true,
           },
         },
+        plugins: {
+          datalabels: {
+            anchor: 'end',
+            align: 'end',
+            color: 'red',
+            font: {
+              weight: 'bold',
+            },
+            formatter: (value: any, context: any) => {
+              return value;
+            },
+          },
+        },
       },
-    },
-  });
-}
-
-
-
-
-ngOnInit() {
-  const compid = '1';
-
-  // Fetch data for all entities
-  this.quote$ = this.quoteservice.fetchallQuote(this.encService.encrypt(compid), '', '','');
-  this.sales$ = this.saleService.fetchallSales(this.encService.encrypt(compid), '', '');  
-  this.purchase$ = this.purchaseService.fetchallPurchase(this.encService.encrypt(compid), '', '');
-
-
-  // Subscribe to the observables and update the counts
-  this.quote$.subscribe(data => {
-    this.totalQuote = data.length;
-    this.updateChartData('totalquotationBarChart', 'Total Quotation', [this.totalQuote]);
-  });
-  this.sales$.subscribe(data => {
-    this.totalsales = data.length;
-    this.updateChartData('totalsaleBarChart', 'Total Sales', [this.totalsales]);
-  });
-  this.purchase$.subscribe(data => {
-    this.totalpurchase = data.length;
-    this.updateChartData('totalpurchaseBarChart', 'Total Purchase', [this.totalpurchase]);
-  });
-
-}
-
-updateChartData(chartId: string, label: string, data: number[]) {
-  const chart = Chart.getChart(chartId);
-  if (chart) {
-    chart.data.labels = [label];
-    chart.data.datasets[0].data = data;
-    chart.update();
+    });
   }
-}
+
+
+
+
+  ngOnInit() {
+    const compid = '1';
+
+    // Fetch data for all entities
+    this.quote$ = this.quoteservice.fetchallQuote(this.encService.encrypt(compid), '', '', '');
+    this.sales$ = this.saleService.fetchallSales(this.encService.encrypt(compid), '', '');
+    this.purchase$ = this.purchaseService.fetchallPurchase(this.encService.encrypt(compid), '', '');
+
+
+    // Subscribe to the observables and update the counts
+    this.quote$.subscribe(data => {
+      this.totalQuote = data.length;
+      this.updateChartData('totalquotationBarChart', 'Total Quotation', [this.totalQuote]);
+    });
+    this.sales$.subscribe(data => {
+      this.totalsales = data.length;
+      this.updateChartData('totalsaleBarChart', 'Total Sales', [this.totalsales]);
+    });
+    this.purchase$.subscribe(data => {
+      this.totalpurchase = data.length;
+      this.updateChartData('totalpurchaseBarChart', 'Total Purchase', [this.totalpurchase]);
+    });
+
+  }
+
+  updateChartData(chartId: string, label: string, data: number[]) {
+    const chart = Chart.getChart(chartId);
+    if (chart) {
+      chart.data.labels = [label];
+      chart.data.datasets[0].data = data;
+      chart.update();
+    }
+  }
+  logout() {
+    // Clear authentication tokens or perform other logout logic
+    // this.authService.logout();
+
+    // Navigate to the login page (assuming your login page has a route named 'login')
+    this.navCtrl.navigateRoot('/login');
+  }
 }
 
 
