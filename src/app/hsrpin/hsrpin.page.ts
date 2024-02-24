d1e1ad28e18151ddd52a5b46816ea98c718dc1cd
Yogesh Sharma <65293781+yogeshsharmao2@users.noreply.github.com>
2024-02-24 15:30:38 +0530

    24/02
    edit api lga rhe hai

--- src/app/hsrpin/hsrpin.page.ts
+++ src/app/hsrpin/hsrpin.page.ts
@@ -107,8 +107,8 @@ export class HsrpinPage implements OnInit {
   vehicle_no: number = 0;
   vehicle_reg_no: number = 0;
   vehicle_reg_date: number = 0;
-  hsrp_front:number=0;
-  hsrp_rear: string = '';
+  hsrp_front: number = 0;
+  hsrp_rear: string='';
   description: string = '';
   hsn_code: string = '';
   quantity: number = 0;
@@ -138,7 +138,6 @@ export class HsrpinPage implements OnInit {
   cdr: any;
   isOpen = false;
   hsrpindata: Hsrpin[] = [{
-    supplier:'',
     barcode: '',
     itemcode: 0,
     itemid: 0,
@@ -163,6 +162,7 @@ export class HsrpinPage implements OnInit {
     basicrate: 0,
     totaltax: 0,
     taxrate: 0,
+    supplier:'',
     tcs_value: 0,
     itemname: '',
     selectedItemId: 0,
@@ -297,7 +297,7 @@ export class HsrpinPage implements OnInit {
   //     vehicle_reg_no: 0,
   //     vehicle_reg_date: 0,
   //     hsrp_front: 0,
-  //     hsrp_rear: 0,
+  //     hsrp_rear: '',
   //     description: '',
   //     hsn_code: '',
   //     mrp: '',
@@ -411,125 +411,63 @@ export class HsrpinPage implements OnInit {
     this.isQuantityPopoverOpen = false;
   }
 
-  // addHsrpin() {
-  //   console.log('addquotewww' + this.hsrpindata.length);
-  //   // You can initialize the new row data here
-  //   let newRow: hsrpinstore = {
-  //     engineframenumber: '',
-  //     supplier: '',
-  //     itemcode: 0,
-  //     hsn_code: '',
-  //     itemname: '',
-  //     quantity: 0,
-  //     vehicle_no: 0,
-  //     vehicle_reg_date: 0,
-  //     hsrp_front: 0,
-  //     hsrp_rear: 0,
-  //     mrp: '',
-  //     netrate: 0,
+ 
 
-  //     basicrate: 0,
-  //     taxrate: 0,
-  //     CGST: 0,
-  //     SGST: 0,
-  //     IGST: 0,
-  //     totaltax: 0,
-  //     total: 0,
-  //     tcs_value: 0,
-  //     grossrate: 0,
-  //     billno: '',
-  //     hsrpdate: '',
-  //     suppliercode: '',
-  //     spler: 0,
-  //     refrence: '',
-  //     refdate: '',
-  //     executive_name: 0,
-  //     totalitemno: 0,
-  //     totalquantity: 0,
-  //     totalgrossamt: 0,
-  //     deliverydate: '',
-  //     deliveryplace: '',
-  //     openingbalance: 0,
-  //     debit: 0,
-  //     closingbalance: 0,
-  //     credit: 0,
-  //     totaldiscountamt: 0,
-  //     totaltaxamount: 0,
-  //     roundoff: 0,
-  //     pretax: 0,
-  //     posttax: 0,
-  //     totalnetamount: 0,
-  //     ttotal: 0,
-  //     quantityPopoverData: this.hsrpindata[0].quantityPopoverData.map(attr => ({ ...attr })),
-  //     frame: 0,
-  //     engine_no: 0,
-  //     vehicle_reg_no: 0,
-  //     billformate: 0,
-  //     barcode: '',
-  //     part: 0,
-  //     description: '',
-  //     discount: 0,
-  //     discountamt: 0,
-  //     taxrate1: 0,
-  //     itemid: 0,
-  //     selectedItemId: 0
-  //   };
-
-
-  //   this.hsrpindata.push();
-  //   // Reset newRow back to an empty object to prepare for the next iteration
-
-  // }
-  addHsrpin() {
-    // Assuming hsrpindata is an array, push a new object with default values
-    this.hsrpindata.push({
-      engineframenumber: '', // provide default values for other properties if needed
-      supplier: '',
-      itemcode: 0,
-      hsn_code: '',
-      itemname: '',
-      quantity: 0, // example default value
-      vehicle_no: 0,
-      vehicle_reg_date: 0,
-      hsrp_front: 0,
-      hsrp_rear: '',
-      mrp: '', // example default value
-      basicrate: 0, // example default value
-      netrate: 0, // example default value
-      grossrate: 0, // example default value
-      taxrate: 0, // example default value
-      CGST: 0, // example default value
-      SGST: 0, // example default value
-      IGST: 0, // example default value
-      totaltax: 0, // example default value
-      tcs_value: 0, // example default value
-      total: 0 // example default value
-      ,
-      selectedItemAttributes: [],
-      barcode: '',
-      part: 0,
-      frame: 0,
-      engine_no: 0,
-      vehicle_reg_no: 0,
-      description: '',
-      taxrate1: 0,
-      discountamt: 0,
-      discount: 0,
-      selectedItemId: 0,
-      itemid: 0,
-      customername: '',
-      quantityPopoverData: [],
-      attribute1: '',
-      attribute2: '',
-      attribute3: '',
-      attribute4: '',
-      attribute5: '',
-      attribute6: '',
-      attribute7: '',
-      attribute8: ''
-    });
-  }
+  addHsrpin() { this.hsrpindata.push({
+    engineframenumber: '', // provide default values for other properties if needed
+    supplier: '',
+    itemcode: 0,
+    hsn_code: '',
+    itemname: '',
+    quantity: 0, // example default value
+    vehicle_no: 0,
+    vehicle_reg_date: 0,
+    hsrp_front: 0,
+    hsrp_rear: '',
+    mrp: '', // example default value
+    basicrate: 0, // example default value
+    netrate: 0, // example default value
+    grossrate: 0, // example default value
+    taxrate: 0, // example default value
+    CGST: 0, // example default value
+    SGST: 0, // example default value
+    IGST: 0, // example default value
+    totaltax: 0, // example default value
+    tcs_value: 0, // example default value
+    total: 0, // example default value,
+    selectedItemAttributes: [],
+    barcode: '',
+    part: 0,
+    frame: 0,
+    engine_no: 0,
+    vehicle_reg_no: 0,
+    description: '',
+    taxrate1: 0,
+    discountamt: 0,
+    discount: 0,
+    selectedItemId: 0,
+    itemid: 0,
+    customername: '',
+    quantityPopoverData: [],
+    attribute1: '',
+    attribute2: '',
+    attribute3: '',
+    attribute4: '',
+    attribute5: '',
+    attribute6: '',
+    attribute7: '',
+    attribute8: ''
   
+  
+  
+  })
+  
+  
+  
+  }
+
+    // Assuming hsrpindata is an array, push a new object with default values
+   
   async onSubmit(form: FormGroup, hsrpindata: Hsrpin[]) {
     const fields = { itemname: this.itemname, quantity: this.quantity, description: this.description }
     // const isValid = await this.formService.validateForm(fields);
